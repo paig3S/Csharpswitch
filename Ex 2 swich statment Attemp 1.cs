c90f88c5135201ed7cef6367f398f1fe52b67d14
paig3S <101837091+paig3S@users.noreply.github.com>
2022-11-23 17:42:35 +0000

    exercise 2 completed

---
+++ Ex 2 swich statment Attemp 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your grade for your Mathmatics Final Exam");
+         
+            string input = Console.ReadLine();
+            char marks = input.ToCharArray()[0];
+            
+            
+
+            //D marks >= 0 & marks <= 49 C marks >= 0 & marks <= 59" B marks >= 0 & marks <= 69  
+            switch (char.ToUpper(marks))
+            {
+                case 'A' :
+                    Console.WriteLine("Your grade is a 70 to 100.");
+                    break;
+                case 'B':
+                    Console.WriteLine("Your grade is a 60 to 69.");
+                    break;
+                case 'C':
+                    Console.WriteLine("Your grade is a 50 to 59.");
+                    break;
+                case 'D':
+                    Console.WriteLine("Your grade is a 40 to 49.");
+                    break;
+                case 'F':
+                    Console.WriteLine("Your grade is 39 to 0.");
+                    break;
+                default :
+                    Console.WriteLine("Not a correct value for marks");
+                    break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }             
+
+}
+        
+    
+
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter your marks");
+            String marks = Console.ReadLine();
+
+
+
+            switch (marks)
+            {
+                case "marks >= 0 & marks <= 39":
+                    Console.WriteLine("Your grade is a F");
+                    break;
+                case "marks >= 40 & marks <= 49":
+                    Console.WriteLine("Your grade is a D");
+                    break;
+                case "marks >= 50 & marks <= 59":
+                    Console.WriteLine("Your grade is a C");
+                    break;
+                case "marks >= 60 & marks <= 69":
+                    Console.WriteLine("Your grade is a B");
+                    break;
+                case "marks >= 70 & marks <= 100":
+                    Console.WriteLine("Your grade is a A");
+                    break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }             
+
+}
+        
+    
+
